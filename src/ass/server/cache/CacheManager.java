78348e142e015a021cca2b4e39a3e89cdc383d8f
Vojta Ciml <VojtaCiml@gmail.com>
2012-04-22 21:14:50 +0200

    Preparing new cache implementation

--- src/ass/server/cache/CacheManager.java
+++ src/ass/server/cache/CacheManager.java
@@ -3,11 +3,15 @@ package ass.server.cache;
 import java.util.ArrayList;
 
 public class CacheManager {
-	private int cacheCapacity = 10; //Capacity in MBytes
+	private int cacheCapacity = 10*1000000; //Capacity in MBytes
 	private ArrayList<CacheObject> cacheObjects = new ArrayList<CacheObject>();
 	
 	public CacheManager(){
 		
+	}
+	
+	public CacheManager(int cacheCapacity){
+		this.cacheCapacity = cacheCapacity;
 	}
 	
 	public boolean isFileCached(String filePath){
