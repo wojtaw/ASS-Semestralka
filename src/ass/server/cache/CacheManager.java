5d15f9accde22f1686398e886a05f3df6ec3e9f4
Vojta Ciml <VojtaCiml@gmail.com>
2012-04-23 10:09:47 +0200

    Cache manager updates

--- src/ass/server/cache/CacheManager.java
+++ src/ass/server/cache/CacheManager.java
@@ -1,10 +1,14 @@
 package ass.server.cache;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CacheManager {
 	private int cacheCapacity = 10*1000000; //Capacity in MBytes
-	private ArrayList<CacheObject> cacheObjects = new ArrayList<CacheObject>();
+	//private ArrayList<CacheObject> cacheObjects = new ArrayList<CacheObject>();
+	//private HashMap<CacheObject, long> cacheObjects = new HashMap();
+	private HashMap cacheObjects = new HashMap();
 	
 	public CacheManager(){
 		
@@ -14,8 +18,23 @@ public class CacheManager {
 		this.cacheCapacity = cacheCapacity;
 	}
 	
-	public boolean isFileCached(String filePath){
+	public boolean isFileCached(File file){
+		
 		return false;
+	}
+	
+	public boolean cacheNewFile(File file){
+		if(isFileCached(file)) return false;
+		cacheObjects.put(file, file.length());
+		return true;
+	}
+	
+	private boolean removeFiles(long neededSize){
+		int gainedSpace = 0;
+		while(gainedSpace < neededSize){
+			
+		}
+		return true
 	}
 	
 	
